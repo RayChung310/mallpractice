7b08b19aa6ff35f27f6a23bc8e41d825d9dbc0cc
RayChung310 <raypc95175300@gmail.com>
2026-02-28 19:34:02 +0800

    init Spring Boot project

---
+++ src/main/java/com/practice/mallpractice/MallpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.mallpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallpracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallpracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/mallpractice/MallpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.mallpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallpracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
